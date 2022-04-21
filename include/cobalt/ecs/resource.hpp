81f57825a6850a45e2e1b2634602b1b645a6210f
Blyschak <stepanblischak@gmail.com>
2022-04-21 15:01:16 +0300

    add resources to registry
    Signed-off-by: Blyschak <stepanblischak@gmail.com>

---
+++ include/cobalt/ecs/resource.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <type_traits>
+
+#include <cobalt/asl/family.hpp>
+
+namespace cobalt::ecs {
+
+/// @brief Type for resource ID
+using resource_id = std::uint32_t;
+
+/// @brief Resource concept
+///
+/// @tparam T Resource type
+template<typename T>
+concept resource = !std::is_reference_v<T> && !std::is_pointer_v<T> && !std::is_const_v<T>;
+
+/// @brief Type for family used to generated resource IDs.
+using resource_family = cobalt::asl::family<struct _resource_family_t, resource_id>;
+
+} // namespace cobalt::ecs
