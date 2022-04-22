f810d4064c1778849c7af275dac4a0981fa3f839
Blyschak <stepanblischak@gmail.com>
2022-04-23 02:44:39 +0300

    add hierarchy utils
    Signed-off-by: Blyschak <stepanblischak@gmail.com>

--- tests/ecs/test_hierarchy.cpp
+++ tests/ecs/test_hierarchy.cpp
@@ -14,28 +14,80 @@ TEST(hierarchy, basic) {
     auto entity11 = registry.create<some_struct>({ "entity11" });
     auto entity12 = registry.create<some_struct>({ "entity12" });
     auto entity121 = registry.create<some_struct>({ "entity121" });
+    auto entity1211 = registry.create<some_struct>({ "entity1211" });
 
-    ecs::entity_set_child(registry, entity1, entity11);
-    ecs::entity_set_child(registry, entity1, entity12);
-    ecs::entity_set_child(registry, entity12, entity121);
+    ecs::hierarchy::set_child(registry, entity1, entity11);
+    ecs::hierarchy::set_child(registry, entity1, entity12);
+    ecs::hierarchy::set_child(registry, entity12, entity121);
+    ecs::hierarchy::set_child(registry, entity121, entity1211);
 
-    ecs::entity_for_each_child<some_struct&>(registry, [&](auto tuple) {
-        auto& parent = std::get<0>(tuple);
-        auto& child = std::get<1>(tuple);
-        // std::cout << "entity " << child.name << " parent " << registry.get<some_struct>(parent.entity).name
-        //           << std::endl;
-    });
+    std::map<std::string, std::set<std::string>> parent_child_relations;
 
-    // std::cout << "==============\n";
+    ecs::hierarchy::for_each_child(registry,
+        [&](std::tuple<const some_struct&> parent_some_struct, std::tuple<const some_struct&> child_some_struct) {
+            auto [parent] = parent_some_struct;
+            auto [child] = child_some_struct;
 
-    ecs::entity_set_child(registry, entity11, entity121);
+            parent_child_relations[parent.name].insert(child.name);
+        });
 
-    ecs::entity_for_each_child<some_struct&>(registry, [&](auto tuple) {
-        auto& parent = std::get<0>(tuple);
-        auto& child = std::get<1>(tuple);
-        // std::cout << "entity " << child.name << " parent " << registry.get<some_struct>(parent.entity).name
-        //           << std::endl;
-    });
+    EXPECT_EQ(parent_child_relations.size(), 3);
+    EXPECT_TRUE(parent_child_relations["entity1"].contains("entity11"));
+    EXPECT_TRUE(parent_child_relations["entity1"].contains("entity12"));
+    EXPECT_TRUE(parent_child_relations["entity12"].contains("entity121"));
+    EXPECT_TRUE(parent_child_relations["entity121"].contains("entity1211"));
 
-    // std::cout << "==============\n";
+    ecs::hierarchy::remove_child(registry, entity1, entity12);
+
+    parent_child_relations.clear();
+    ecs::hierarchy::for_each_child(registry,
+        [&](std::tuple<const some_struct&> parent_some_struct, std::tuple<const some_struct&> child_some_struct) {
+            auto [parent] = parent_some_struct;
+            auto [child] = child_some_struct;
+
+            parent_child_relations[parent.name].insert(child.name);
+        });
+    EXPECT_EQ(parent_child_relations.size(), 3);
+    EXPECT_TRUE(parent_child_relations["entity1"].contains("entity11"));
+    EXPECT_TRUE(parent_child_relations["entity12"].contains("entity121"));
+    EXPECT_TRUE(parent_child_relations["entity121"].contains("entity1211"));
+
+    ecs::hierarchy::remove_child(registry, entity121, entity1211);
+
+    parent_child_relations.clear();
+    ecs::hierarchy::for_each_child(registry,
+        [&](std::tuple<const some_struct&> parent_some_struct, std::tuple<const some_struct&> child_some_struct) {
+            auto [parent] = parent_some_struct;
+            auto [child] = child_some_struct;
+
+            parent_child_relations[parent.name].insert(child.name);
+        });
+    EXPECT_EQ(parent_child_relations.size(), 2);
+    EXPECT_TRUE(parent_child_relations["entity1"].contains("entity11"));
+    EXPECT_TRUE(parent_child_relations["entity12"].contains("entity121"));
+
+    ecs::hierarchy::remove_child(registry, entity1, entity11);
+
+    parent_child_relations.clear();
+    ecs::hierarchy::for_each_child(registry,
+        [&](std::tuple<const some_struct&> parent_some_struct, std::tuple<const some_struct&> child_some_struct) {
+            auto [parent] = parent_some_struct;
+            auto [child] = child_some_struct;
+
+            parent_child_relations[parent.name].insert(child.name);
+        });
+    EXPECT_EQ(parent_child_relations.size(), 1);
+    EXPECT_TRUE(parent_child_relations["entity12"].contains("entity121"));
+
+    ecs::hierarchy::remove_child(registry, entity12, entity121);
+
+    parent_child_relations.clear();
+    ecs::hierarchy::for_each_child(registry,
+        [&](std::tuple<const some_struct&> parent_some_struct, std::tuple<const some_struct&> child_some_struct) {
+            auto [parent] = parent_some_struct;
+            auto [child] = child_some_struct;
+
+            parent_child_relations[parent.name].insert(child.name);
+        });
+    EXPECT_TRUE(parent_child_relations.empty());
 }
