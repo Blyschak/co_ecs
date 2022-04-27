b338868869b840beddd3c68b84918c5e107d02d9
Blyschak <stepanblischak@gmail.com>
2022-04-27 17:30:21 +0300

    add lua tests
    Signed-off-by: Blyschak <stepanblischak@gmail.com>

--- include/cobalt/scripting/lua_engine.hpp
+++ include/cobalt/scripting/lua_engine.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cobalt/ecs/registry.hpp>
+
 #include <sol/sol.hpp>
 
 namespace cobalt::scripting {
@@ -9,6 +11,41 @@ class lua_engine {
 public:
     /// @brief Construct a new lua engine object
     lua_engine();
+
+    /// @brief Execute script passed in this function
+    ///
+    /// @param script Lua script code
+    void script(const std::string& script);
+
+    /// @brief Register component
+    ///
+    /// @tparam C Component type
+    /// @tparam Ctors Constructor types
+    /// @param name Component name
+    /// @param args Args to pass to new_usertype<C>
+    template<ecs::component C, typename... Ctors>
+    void register_component(std::string name, auto&&... args) {
+        lua.new_usertype<C>(
+            name,
+            sol::constructors<Ctors...>(),
+            "id",
+            []() { return ecs::component_family::id<C>; },
+            std::forward<decltype(args)>(args)...);
+    }
+
+    /// @brief Get the state object
+    ///
+    /// @return sol::state&
+    [[nodiscard]] sol::state& state() noexcept {
+        return lua;
+    }
+
+    /// @brief Get the state object
+    ///
+    /// @return const sol::state&
+    [[nodiscard]] const sol::state& state() const noexcept {
+        return lua;
+    }
 
 private:
     sol::state lua;
