a12662f0efdd61c2559886681f43d103d9568ed7
Blyschak <stepanblischak@gmail.com>
2022-05-09 18:49:09 +0300

    add 1M benchmarks
    Signed-off-by: Blyschak <stepanblischak@gmail.com>

--- benchmarks/ecs/registry.cpp
+++ benchmarks/ecs/registry.cpp
@@ -18,19 +18,34 @@ struct velocity {
     int y{};
 };
 
-static void bm_entity_creation(benchmark::State& state) {
-    auto registry = cobalt::ecs::registry();
-    for (auto _ : state) {
-        registry.create<position, rotation, velocity>({}, {}, {});
+static void setup_registry_for_iteration(cobalt::ecs::registry& registry, std::size_t count) {
+    for (int i = 0; i < count; i++) {
+        registry.create<position, rotation>({ i, i * 2 }, { i * 3, i });
     }
 }
 
+template<std::size_t N>
+static void bm_entity_creation(benchmark::State& state) {
+    auto registry = cobalt::ecs::registry();
+    for (auto _ : state) {
+        for (auto i = 0; i < N; i++) {
+            registry.create<position, rotation, velocity>({}, {}, {});
+        }
+    }
+}
+
+template<std::size_t N>
 static void bm_entity_archetype_change(benchmark::State& state) {
     auto registry = cobalt::ecs::registry();
-    auto entity = registry.create<position, rotation>({}, {});
+    auto entity = cobalt::ecs::entity::invalid();
+    for (auto i = 0; i < 1000000; i++) {
+        entity = registry.create<position, rotation>({}, {});
+    }
     for (auto _ : state) {
-        registry.set<velocity>(entity);
-        registry.remove<velocity>(entity);
+        for (auto i = 0; i < N; i++) {
+            registry.set<velocity>(entity);
+            registry.remove<velocity>(entity);
+        }
     }
 }
 
@@ -50,11 +65,10 @@ static void bm_entity_get_component(benchmark::State& state) {
     }
 }
 
+template<std::size_t N>
 static void bm_entity_iterate_component(benchmark::State& state) {
     auto registry = cobalt::ecs::registry();
-    for (int i = 0; i < 20; i++) {
-        registry.create<position, rotation>({ i, i * 2 }, { i * 3, i });
-    }
+    setup_registry_for_iteration(registry, N);
 
     int sum = 0;
     for (auto _ : state) {
@@ -66,11 +80,10 @@ static void bm_entity_iterate_component(benchmark::State& state) {
     benchmark::DoNotOptimize(sum);
 }
 
+template<std::size_t N>
 static void bm_entity_iterate_component_with_view(benchmark::State& state) {
     auto registry = cobalt::ecs::registry();
-    for (int i = 0; i < 20; i++) {
-        registry.create<position, rotation>({ i, i * 2 }, { i * 3, i });
-    }
+    setup_registry_for_iteration(registry, N);
 
     int sum = 0;
     for (auto _ : state) {
@@ -81,11 +94,10 @@ static void bm_entity_iterate_component_with_view(benchmark::State& state) {
     benchmark::DoNotOptimize(sum);
 }
 
+template<std::size_t N>
 static void bm_entity_iterate_component_with_system(benchmark::State& state) {
     auto registry = cobalt::ecs::registry();
-    for (int i = 0; i < 20; i++) {
-        registry.create<position, rotation>({ i, i * 2 }, { i * 3, i });
-    }
+    setup_registry_for_iteration(registry, N);
 
     int sum = 0;
     cobalt::ecs::system system(registry, [&sum](cobalt::ecs::view<const position&, const rotation&> view) {
@@ -99,10 +111,16 @@ static void bm_entity_iterate_component_with_system(benchmark::State& state) {
     benchmark::DoNotOptimize(sum);
 }
 
-BENCHMARK(bm_entity_creation);
+
+BENCHMARK(bm_entity_creation<1>);
+BENCHMARK(bm_entity_creation<1000000>)->Unit(benchmark::kSecond);
 BENCHMARK(bm_entity_set_component);
 BENCHMARK(bm_entity_get_component);
-BENCHMARK(bm_entity_archetype_change);
-BENCHMARK(bm_entity_iterate_component);
-BENCHMARK(bm_entity_iterate_component_with_view);
-BENCHMARK(bm_entity_iterate_component_with_system);
+BENCHMARK(bm_entity_archetype_change<1>);
+BENCHMARK(bm_entity_archetype_change<1000000>)->Unit(benchmark::kSecond);
+BENCHMARK(bm_entity_iterate_component<20>);
+BENCHMARK(bm_entity_iterate_component_with_view<20>);
+BENCHMARK(bm_entity_iterate_component_with_system<20>);
+BENCHMARK(bm_entity_iterate_component<1000000>)->Unit(benchmark::kMicrosecond);
+BENCHMARK(bm_entity_iterate_component_with_view<1000000>)->Unit(benchmark::kMicrosecond);
+BENCHMARK(bm_entity_iterate_component_with_system<1000000>)->Unit(benchmark::kMicrosecond);
