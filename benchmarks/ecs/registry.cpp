352e67c09fcf27c77b22b6efa93a05e5e6dca17e
Blyschak <stepanblischak@gmail.com>
2024-05-08 19:27:55 +0300

    extend benchmarks with system iterations
    Signed-off-by: Blyschak <stepanblischak@gmail.com>

--- benchmarks/ecs/registry.cpp
+++ benchmarks/ecs/registry.cpp
@@ -59,6 +59,8 @@ constexpr std::size_t operator"" _bytes_each(unsigned long long int number) {
     return number;
 }
 
+std::uint64_t sum{ 0 };
+
 // Creates an entity with the given number of components
 template<std::size_t N, std::size_t S = 0>
 static void bm_entity_creation_with(benchmark::State& state) {
@@ -99,13 +101,11 @@ static void bm_1M_entities_iteration(benchmark::State& state) {
         create_entity();
     }
 
-    std::uint64_t sum{ 0 };
-
     auto bench_func = [&]() {
         std::apply(
             [&]<typename... Args>(Args&&... args) {
                 registry.view<const Args&...>().each(
-                    [&sum](auto&&... args) { sum += (static_cast<std::uint8_t>(args.data[0]) + ...); });
+                    [](auto&&... args) { sum += (static_cast<std::uint8_t>(args.data[0]) + ...); });
             },
             components_tuple{});
     };
@@ -114,6 +114,46 @@ static void bm_1M_entities_iteration(benchmark::State& state) {
 
     for (auto _ : state) {
         bench_func();
+    }
+
+    const std::size_t size = std::is_empty_v<components_tuple> ? 0 : sizeof(components_tuple);
+    state.SetBytesProcessed(int64_t(state.iterations()) * size * entities_count);
+}
+
+
+// Iterate 1M entity with given number of components
+template<std::size_t N, std::size_t S>
+static void bm_1M_entities_iteration_with_system(benchmark::State& state) {
+    const auto entities_count = 1000000;
+    using components_tuple = typename components_generator<foo_creator<S>, N>::type;
+
+    auto registry = co_ecs::registry();
+
+    auto create_entity = [&]() {
+        std::apply([&]<typename... Args>(Args&&... args) { registry.create<Args...>(std::forward<Args>(args)...); },
+            components_tuple{});
+    };
+
+    for (auto _ : std::ranges::iota_view{ 0, entities_count }) {
+        create_entity();
+    }
+
+    auto system = std::apply(
+        [&]<typename... Args>(Args&&... args) {
+            auto func = [](co_ecs::view<const Args&...> v) {
+                v.each([](auto&&... args) { sum += (static_cast<std::uint8_t>(args.data[0]) + ...); });
+            };
+
+            return std::make_unique<co_ecs::system<decltype(func)>>(std::forward<decltype(func)>(func));
+        },
+        components_tuple{});
+
+    auto system_executor = system->create_executor(registry, nullptr);
+
+    benchmark::DoNotOptimize(sum);
+
+    for (auto _ : state) {
+        system_executor->run();
     }
 
     const std::size_t size = std::is_empty_v<components_tuple> ? 0 : sizeof(components_tuple);
@@ -130,3 +170,8 @@ BENCHMARK(bm_1M_entities_iteration<1_components, 64_bytes_each>)->Unit(benchmark
 BENCHMARK(bm_1M_entities_iteration<2_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
 BENCHMARK(bm_1M_entities_iteration<4_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
 BENCHMARK(bm_1M_entities_iteration<8_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
+
+BENCHMARK(bm_1M_entities_iteration_with_system<1_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
+BENCHMARK(bm_1M_entities_iteration_with_system<2_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
+BENCHMARK(bm_1M_entities_iteration_with_system<4_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
+BENCHMARK(bm_1M_entities_iteration_with_system<8_components, 64_bytes_each>)->Unit(benchmark::kMillisecond);
